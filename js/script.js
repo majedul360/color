70c95367c58d20f5835a435abf9c42dd232f54ac
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-02-16 19:02:40 +0600

    navbar  highlight on scroll done

--- js/script.js
+++ js/script.js
@@ -13,3 +13,35 @@ document.querySelector(".menu-bar").addEventListener("click", () => {
 
   document.querySelector("nav").classList.toggle("active");
 });
+
+// scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector(".menu-bar span:nth-child(2)")
+    .classList.remove("active");
+  document
+    .querySelector(".menu-bar span:nth-child(1)")
+    .classList.remove("active");
+  document
+    .querySelector(".menu-bar span:nth-child(3)")
+    .classList.remove("active");
+  nav.classList.remove("active");
+
+  let currentSection = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 4) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.classList.contains(currentSection)) {
+      link.classList.add("active");
+    }
+  });
+});
